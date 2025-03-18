59412ce000f0fdad579bbb9bc9287eea2d0ddf13
Marcus Kaa <sucram@ls-site.de>
2025-03-18 06:57:23 +0100

    feat: implement ProjectInput class for form handling

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,21 @@
-// Code goes here!
+class ProjectInput {
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLFormElement;
+
+    constructor() {
+        this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
+        this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild as HTMLFormElement;
+
+        this.attatch();
+    }
+
+    private attatch() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    }
+}
+
+const prjInput = new ProjectInput();

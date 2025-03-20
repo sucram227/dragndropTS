1ce0f4f2e67fabefe46e60786c48d8138f2840e0
Marcus Kaa <sucram@ls-site.de>
2025-03-20 11:14:31 +0100

    feat: implement project status update on drop event in ProjectList component

--- src/app.ts
+++ src/app.ts
@@ -34,6 +34,18 @@ class ProjectState extends State<Project> {
 
         this.projects.push(newProject);
 
+        this.updateListeners();
+    }
+
+    moveProject(projectId: string, newStatus: ProjectStatus) {
+        const project = this.projects.find(prj => prj.id === projectId);
+        if (project && project.status !== newStatus) {
+            project.status = newStatus;
+            this.updateListeners();
+        }
+    }
+
+    private updateListeners() {
         for (const listenerFn of this.listeners) {
             listenerFn(this.projects.slice());
         }
@@ -228,8 +240,10 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> implements Drag
         }
     }
 
+    @autoBind
     dropHandler(event: DragEvent): void {
-        console.log(event);
+        const projectId = event.dataTransfer!.getData('text/plain');
+        projectState.moveProject(projectId, this.type === 'active' ? ProjectStatus.Active : ProjectStatus.Finished);
     }
 
     @autoBind

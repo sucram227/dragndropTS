3af91bd2ce8e0c03a913faea94286a62dda145ed
Marcus Kaa <sucram@ls-site.de>
2025-03-20 09:57:34 +0100

    feat: implement ProjectItem class for individual project rendering in the project list

--- src/app.ts
+++ src/app.ts
@@ -134,6 +134,26 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
     abstract renderContent(): void;
 }
 
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+    private project: Project;
+
+    constructor(hostId: string, project: Project) {
+        super('single-project', hostId, false, project.id);
+        this.project = project;
+
+        this.configure();
+        this.renderContent();
+    }
+
+    configure() {
+    }
+
+    renderContent() {
+        this.element.querySelector('h2')!.textContent = this.project.title;
+        this.element.querySelector('h3')!.textContent = this.project.people.toString();
+        this.element.querySelector('p')!.textContent = this.project.description;
+    }
+}
 
 class ProjectList extends Component<HTMLDivElement, HTMLElement> { 
     assignedProjects: Project[];
@@ -171,10 +191,9 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
     private renderProjects() { 
         const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
         listEl.innerHTML = '';          //Rerenders the entire project list, prevents duplicates
+
         for (const prjItem of this.assignedProjects) {
-            const listItem = document.createElement('li');
-            listItem.textContent = prjItem.title;
-            listEl.appendChild(listItem);
+            new ProjectItem(this.element.querySelector('ul')!.id, prjItem);
         }
     }
 }
